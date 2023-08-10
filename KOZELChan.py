68d3bb277ef2a81eaceb573201963930f4a16c0d
KOZEL <141586620+KOZELChan@users.noreply.github.com>
2023-08-10 23:08:04 +0700

    Update KOZELChan.py

--- KOZELChan.py
+++ KOZELChan.py
@@ -29,6 +29,7 @@ proxsi=open('socksku.txt','r').read().splitlines()
 
 ###----------[ USER AGENT ]----------###
 for agenku in range(10000):
+for agenku in range(10000):
 	a='Mozilla/5.0 (Linux; Android'
 	b=random.choice(['10','11','12'])
 	c='Redmi K30 Pro Zoom Edition Build/SKQ1.211006.001; wv)'
@@ -54,6 +55,53 @@ for t in range(10000):
 	uakuh=f'{a} {b}; {c} {d}{e}.{f}.{g}.{h} {i}'
 	ugen.append(uakuh)
 	
+	a='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['8','8.1.0','9','10','11'])
+	c='HTC One M8 Pro Build/MRA58K)'
+	d='AppleWebKit/601.44 (KHTML, like Gecko)  Chrome/48.0.2678.228'
+	e=random.randrange(83,103)
+	f='0'
+	g=random.randrange(4200,4900)
+	h=random.randrange(40,150)
+	i='Mobile Safari/600.5'
+	uakuh=f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}'
+	ugen.append(uakuh)
+	
+	a='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['8','9','10','11','12'])
+	c='SM-N8000 Build/JZO54K)'
+	d='AppleWebKit/534.5 (KHTML, like Gecko)  Chrome/47.0.2892.259'
+	e=random.randrange(83,103)
+	f='0'
+	g=random.randrange(4200,4900)
+	h=random.randrange(40,150)
+	i='Mobile Safari/602.4'
+	uakuh=f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}'
+	ugen.append(uakuh)
+	
+	a='Mozilla/5.0 (Linux; U; Android'
+	b=random.choice(['8','9','10','11','12'])
+	c='MOTOROLA MOTOG Build/KVT49L)'
+	d='AppleWebKit/601.8 (KHTML, like Gecko)  Chrome/47.0.2725.317'
+	e=random.randrange(83,103)
+	f='0'
+	g=random.randrange(4200,4900)
+	h=random.randrange(40,150)
+	i='Mobile Safari/601.4'
+	uakuh=f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}'
+	ugen.append(uakuh)
+	
+	a='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['8','9','10','11','12'])
+	c='LG-LU6500 Build/IMM76L)'
+	d='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.107'
+	e=random.randrange(83,103)
+	f='0'
+	g=random.randrange(4200,4900)
+	h=random.randrange(40,150)
+	i='Mobile Safari/537.36 OPR/29.0.1809.93516'
+	uakuh=f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}'
+	usragent.append(uakuh)
 	
 	###---------------[USER AGENT 2]---------------###
 P = '\x1b[1;97m'
@@ -504,6 +552,7 @@ AGAR TERHINDAR DARI SPAM IP
 						pwv.append(frs+'123')
 						pwv.append(frs+'1234')
 						pwv.append(frs+'12345')
+						pwv.append(kamu nanya)
 				else:
 					if len(frs)<3:
 						pwv.append(nmf)
@@ -512,6 +561,7 @@ AGAR TERHINDAR DARI SPAM IP
 						pwv.append(frs+'123')
 						pwv.append(frs+'1234')
 						pwv.append(frs+'12345')	
+						pwv.append(kamu nanya)
 				if 'ya' in pwt:
 					for xpwn in pwn:
 						pwv.append(xpwn)
